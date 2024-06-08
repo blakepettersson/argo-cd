7cc7b17a523a389a24a10b5738a703469bfd81b5
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2024-06-07 21:43:45 -0400

    minor fixes
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>

--- server/repository/repository.go
+++ server/repository/repository.go
@@ -3,8 +3,9 @@ package repository
 import (
 	"context"
 	"fmt"
-	"github.com/argoproj/argo-cd/v2/util/git"
 	"reflect"
+	"sort"
+	"strings"
 
 	"github.com/argoproj/gitops-engine/pkg/utils/kube"
 	"github.com/argoproj/gitops-engine/pkg/utils/text"
@@ -26,6 +27,7 @@ import (
 	"github.com/argoproj/argo-cd/v2/util/argo"
 	"github.com/argoproj/argo-cd/v2/util/db"
 	"github.com/argoproj/argo-cd/v2/util/errors"
+	"github.com/argoproj/argo-cd/v2/util/git"
 	"github.com/argoproj/argo-cd/v2/util/io"
 	"github.com/argoproj/argo-cd/v2/util/rbac"
 	"github.com/argoproj/argo-cd/v2/util/settings"
@@ -209,6 +211,11 @@ func (s *Server) ListRepositories(ctx context.Context, q *repositorypkg.RepoQuer
 	if err != nil {
 		return nil, err
 	}
+	sort.Slice(items, func(i, j int) bool {
+		first := items[i]
+		second := items[j]
+		return strings.Compare(fmt.Sprintf("%s/%s", first.Project, first.Repo), fmt.Sprintf("%s/%s", second.Project, second.Repo)) < 0
+	})
 	return &appsv1.RepositoryList{Items: items}, nil
 }
 
@@ -482,7 +489,7 @@ func (s *Server) DeleteRepository(ctx context.Context, q *repositorypkg.RepoQuer
 	}
 
 	// invalidate cache
-	if err := s.cache.SetRepoConnectionState(repo.Repo, repo.Project, nil); err == nil {
+	if err := s.cache.SetRepoConnectionState(repo.Repo, repo.Project, nil); err != nil {
 		log.Errorf("error invalidating cache: %v", err)
 	}
 
